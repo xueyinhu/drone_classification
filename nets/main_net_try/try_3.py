775b487a81dfc1bfe80698f37e0b8e777163635d
xueyinhu <13453894389@163.com>
2024-01-20 23:43:37 +0800

    that

---
+++ nets/main_net_try/try_3.py
@@ -0,0 +1,12 @@
+import tensorflow as tf
+from tensorflow.python.keras import Model, Sequential
+from tensorflow.python.keras.layers import Conv2D
+
+
+class Inc1(Model):
+    def __init__(self):
+        super(Inc1, self)._init__()
+        
+
+    def call(self, x):
+        pass
